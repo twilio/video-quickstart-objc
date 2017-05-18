520ce8e8cb5baba3deeb786c739786895fa2f311
Chris Eagleston <ceagleston@twilio.com>
2017-05-17 17:25:46 -0700

    Add AVPlayerExample project (take 2).

---
+++ AVPlayerExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVPlayerExample
+//
+//  Created by Chris Eagleston on 5/16/17.
+//  Copyright © 2017 Twilio Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AVPlayerExample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AVPlayerExample
+//
+//  Created by Chris Eagleston on 5/16/17.
+//  Copyright © 2017 Twilio Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
