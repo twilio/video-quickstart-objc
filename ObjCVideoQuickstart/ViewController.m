9185f4498af70756b737b5313b8a9b3547b0b47a
Chris Eagleston <ceagleston@twilio.com>
2017-05-17 18:42:08 -0700

    Minor message UI improvements.
    * Logging a message uses both NSLog and sets messageLabel
    * Log the version using logMessage for consistency

--- ObjCVideoQuickstart/ViewController.m
+++ ObjCVideoQuickstart/ViewController.m
@@ -46,7 +46,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"version = %@",[TwilioVideo version]);
+
+    [self logMessage:[NSString stringWithFormat:@"TwilioVideo v%@", [TwilioVideo version]]];
+
     // Configure access token manually for testing, if desired! Create one manually in the console
     self.accessToken = @"TWILIO_ACCESS_TOKEN";
     
@@ -264,6 +266,7 @@
 }
 
 - (void)logMessage:(NSString *)msg {
+    NSLog(@"%@", msg);
     self.messageLabel.text = msg;
 }
 
